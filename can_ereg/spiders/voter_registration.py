1bc83d7e984e7b3155a02ff06faee3ba81d429d0
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-11-18 17:03:03 -0500

    Manually solved captcha.

--- can_ereg/spiders/voter_registration.py
+++ can_ereg/spiders/voter_registration.py
@@ -118,12 +118,38 @@ class VoterRegistrationSpider(scrapy.Spider):
         yield FormRequest(
                 response.url,
                 formdata=formdata,
-                callback=self.render_url,
+                callback=self.solve_captcha,
                 )
 
-    def render_url(self, response):
+    def solve_captcha(self, response):
         # Can fetch this a few time and get different randomly-generated captchas to help make a better guess.
         # See: https://ereg.elections.ca/Telerik.Web.UI.WebResource.axd?type=rca&isc=true&guid=2d918a7f-09cb-4e0e-92e2-125d4ddb156a
-        captcha_rel_link = response.css('#ctl00_ContentPlaceHolder1_RadCaptcha1_CaptchaImage::attr(src)').extract_first()
-        logger.info('CAPTCHA URL: https://ereg.elections.ca/' + captcha_rel_link)
+        #captcha_rel_link = response.css('#ctl00_ContentPlaceHolder1_RadCaptcha1_CaptchaImage::attr(src)').extract_first()
+        open_in_browser(response)
+
+        self.crawler.engine.pause()
+        captcha = input("Captcha: ")
+        self.crawler.engine.unpause()
+
+        formdata = {
+                '__EVENTTARGET': '',
+                '__EVENTARGUMENT': '',
+                '__VIEWSTATE': response.css('#__VIEWSTATE::attr(value)').extract_first(),
+                '__EVENTVALIDATION': response.css('#__EVENTVALIDATION::attr(value)').extract_first(),
+                'ctl00$ContentPlaceHolder1$txtCaptcha': captcha,
+                }
+        yield FormRequest.from_response(
+                response,
+                formdata=formdata,
+                formcss='#form1',
+                clickdata={'name':'ctl00$ContentPlaceHolder1$NavButton$BtnNextDelay'},
+                callback=self.parse_vote_eligibility,
+                )
+
+    def parse_vote_eligibility(self, response):
+        open_in_browser(response)
+        text = response.css('.result > .firstchild > h2::text').extract_first().strip()
+        logger.info(text)
+
+    def render_url(self, response):
         open_in_browser(response)
