f061777629faf4406a357ba4e905a83eb404fbe7
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-11-24 20:57:58 -0500

    Allowed REDIS_URL to come from envvar.

--- tasks.py
+++ tasks.py
@@ -1,13 +1,15 @@
-from celery import Celery
-
 import scrapy
+
+from celery import Celery
+from os import environ
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from scrapyscript import Job, Processor
 from can_ereg.spiders.voter_registration import VoterRegistrationSpider
 
 
-app = Celery('tasks', backend='redis://localhost:6379/1', broker='redis://localhost:6379/0')
+redis_url = environ['REDIS_URL']
+app = Celery('tasks', backend='{}/1'.format(redis_url), broker='{}/0'.format(redis_url))
 
 @app.task
 def check_registration(voter_data=None):
