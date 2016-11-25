f8558ab1ddf9618f4fdab1ed9305887338686a87
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-11-25 01:43:41 -0500

    Removed name from example.py

--- example.py
+++ example.py
@@ -1,15 +1,6 @@
 from tasks import check_registration
 
 voter_data = {
-        'first_name': 'Coleman',
-        'last_name': 'Black',
-        'birth_date': '1985-12-25',
-        'postal_code': 'K1Z7X8',
-        'street_number': '1456',
-        'unit_number': '',
-        }
-
-voter_data = {
         'first_name': 'Ann',
         'last_name': 'Connolly',
         'birth_date': '1964-5-1',
