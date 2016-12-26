272922b282f80be3b50a6f56528b3f18a6233688
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-12-26 03:02:37 -0400

    Allow host to be set via envvar. Normalize address unit. Return geocode data on check creation.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import connexion
+import json
 import logging
+import re
 
 from connexion import NoContent
 from flask import redirect
@@ -9,6 +11,8 @@ from tasks import check_registration
 from tasks import DEFAULT_STATE
 
 
+host = environ.get('HOST', 'https://can-ereg-api.herokuapp.com')
+
 def extract_geo_component(response, component):
     name = [c['long_name'] for c in response.raw['address_components'] if component in c['types']]
     return '' if not name else name[0]
@@ -16,19 +20,60 @@ def extract_geo_component(response, component):
 def create_check(**data):
     # TODO: Fix in spider so this isn't required.
     data.update({'unit_number': ''})
+    geocoder_res = None
 
     if data.get('full_address'):
-        g = geocoders.GoogleV3()
-        response = g.geocode(data['full_address'], components={'country':'CA'})
-        address_data = {
-                'postal_code': extract_geo_component(response, 'postal_code'),
-                'street_number': extract_geo_component(response, 'street_number'),
-                'unit_number': extract_geo_component(response, 'subpremise'),
+        geocoder_res = geocoder_response(data['full_address'])
+        geocoder_data = {
+                'postal_code': extract_geo_component(geocoder_res, 'postal_code'),
+                'street_name': extract_geo_component(geocoder_res, 'route'),
+                'street_number': extract_geo_component(geocoder_res, 'street_number'),
+                'unit_number': extract_geo_component(geocoder_res, 'subpremise'),
                 }
-        data.update(address_data)
+        data.update(geocoder_data)
+
     task = check_registration.delay(data)
 
-    return NoContent, 202, {'Location': 'https://can-ereg-api.herokuapp.com/v1/checks/{}'.format(task.id)}
+    data = {
+            'check_id': task.id,
+            'geocoded_data': geocoder_res.raw,
+            'submitted_data': data,
+            }
+
+    headers = {
+        'Location': '{}/v1/checks/{}'.format(host, task.id),
+        }
+
+    return data, 202, headers
+
+def geocoder_response(full_address):
+    full_address = normalize_address(full_address)
+    g = geocoders.GoogleV3()
+    response = g.geocode(full_address, components={'country':'CA'})
+
+    return response
+
+def normalize_address(full_address):
+    """
+    Converts a Toronto-specific shorthand for building unit numbers
+    into a format that the GMaps geocoder understands.
+
+    GMaps geocoder expects building units to be formatted like so:
+
+        719 Bloor St W #117, Toronto, ON
+        719 Bloor St W, Toronto, ON #117
+
+    It does not know how to classify this shorthand:
+
+        117-719 Bloor St W, Toronto, ON
+    """
+    match = re.match('^(?P<unit>\d+[A-Za-z]?)-(?P<rest>.+)$', full_address)
+    if match:
+        addr_unit = match.group('unit')
+        addr_rest = match.group('rest')
+        full_address = '{} #{}'.format(addr_rest, addr_unit)
+
+    return full_address
 
 def get_check(check_id):
     task = check_registration.AsyncResult(check_id)
